0c53ac1578ee724661c9a343d999ef21ae83d821
npweber <npweber@npwdev.net>
2025-08-09 13:34:18 -0700

    Added jest testing, Customized index page and layout.tsx

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,28 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  async headers() {
+    return [
+      {
+        // Apply these headers to all routes
+        source: '/(.*)',
+        headers: [
+          {
+            key: 'Access-Control-Allow-Origin',
+            value: 'http://192.168.0.152:3000',
+          },
+          {
+            key: 'Access-Control-Allow-Methods',
+            value: 'GET, POST, PUT, DELETE, OPTIONS',
+          },
+          {
+            key: 'Access-Control-Allow-Headers',
+            value: 'Content-Type, Authorization',
+          },
+        ],
+      },
+    ]
+  },
 };
 
 export default nextConfig;
---
+++ src/app/page.tsx
@@ -0,0 +1,8 @@
+export default async function Home() {
+  return (
+    // Temporary page to test the app
+    <div>
+      <h1>Hello World</h1>
+    </div>
+  );
+}
